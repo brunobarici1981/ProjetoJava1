79a68c3a022b0a7069f0c63392a3e4a02dd2b0a7
Bruno <you@example.com>
2024-06-04 10:28:25 -0300

    Adicionado o atributo "idade" a classe pessoa

--- src/projetoJavaCurso/Pessoa.java
+++ src/projetoJavaCurso/Pessoa.java
@@ -3,6 +3,15 @@ package projetoJavaCurso;
 public class Pessoa {
 
 	private String nome,sobrenome;
+	private int idade;
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 	public String getNome() {
 		return nome;
