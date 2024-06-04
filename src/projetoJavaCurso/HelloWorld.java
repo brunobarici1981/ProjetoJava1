33ce5d6a9ea1fa43ab9c1968f9f1504cdb648f3d
Bruno <you@example.com>
2024-06-04 10:23:21 -0300

    Criacao e alteraçao classe pessoa

--- src/projetoJavaCurso/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package projetoJavaCurso;
-
-public class HelloWorld {
- public static void main(String[] args) {
-	 System.out.print("Helo world");
- }
-}
--- src/projetoJavaCurso/Pessoa.java
+++ src/projetoJavaCurso/Pessoa.java
@@ -2,4 +2,22 @@ package projetoJavaCurso;
 
 public class Pessoa {
 
+	private String nome,sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
 }
