c2cecbceb9bfe2e1267ca10878db57d28c7d3c43
Bruno <you@example.com>
2024-06-05 10:30:45 -0300

    Instacia de Aluno na classe Pessoa

--- src/Operacoes_com_objeto_Aluno/Aluno.java
+++ src/Operacoes_com_objeto_Aluno/Aluno.java
@@ -3,8 +3,8 @@ import projetoJavaCurso.Pessoa;
 
 public class Aluno{
 
-	String nome, registroGeral, numeroCpf, nomeMae, nomePai, dataNascimento, dataMatricula, nomeEscola,
-			SerieMatriculado;
+	private String nome, registroGeral, numeroCpf, nomeMae, nomePai, dataNascimento, dataMatricula, nomeEscola,
+			serieMatriculado;
 	int idade;
 	
 	public Aluno() {
@@ -76,11 +76,11 @@ public class Aluno{
 	}
 
 	public String getSerieMatriculado() {
-		return SerieMatriculado;
+		return serieMatriculado;
 	}
 
 	public void setSerieMatriculado(String serieMatriculado) {
-		SerieMatriculado = serieMatriculado;
+		serieMatriculado = serieMatriculado;
 	}
 
 	public int getIdade() {
@@ -94,32 +94,30 @@ public class Aluno{
 	
 	public Aluno(String nome, String registroGeral, String numeroCpf, String nomeMae, String nomePai,
 			String dataNascimento, String dataMatricula, String nomeEscola, String serieMatriculado, int idade) {
-		super();
-		this.nome = nome;
-		this.registroGeral = registroGeral;
-		this.numeroCpf = numeroCpf;
-		this.nomeMae = nomeMae;
-		this.nomePai = nomePai;
-		this.dataNascimento = dataNascimento;
-		this.dataMatricula = dataMatricula;
-		this.nomeEscola = nomeEscola;
-		this.SerieMatriculado = serieMatriculado;
-		this.idade = idade;
+		setNome(nome);
+		setRegistroGeral(registroGeral);
+		setNumeroCpf(numeroCpf);
+		setNomeMae(nomeMae);
+		setNomePai(nomePai);
+		setDataNascimento(dataNascimento);
+		setDataMatricula(dataMatricula);
+		setNomeEscola(nomeEscola);
+		setSerieMatriculado(serieMatriculado);
+		setIdade(idade);
 	}
 	
 @Override
 	public String toString() {
 		return "Aluno \nnome=" + nome + "\nregistroGeral=" + registroGeral + "\nnumeroCpf=" + numeroCpf + "\nnomeMae="
 				+ nomeMae + "\nnomePai=" + nomePai + "\ndataNascimento=" + dataNascimento + "\ndataMatricula="
-				+ dataMatricula + "\nnomeEscola=" + nomeEscola + "\nSerieMatriculado=" + SerieMatriculado + "\nidade="
+				+ dataMatricula + "\nnomeEscola=" + nomeEscola + "\nSerieMatriculado=" + serieMatriculado + "\nidade="
 				+ idade;
 	}
 
 
 public static void main(String[] args) {
 	  
-		Aluno aluno1 = new Aluno("Bruno","43899030","3112245","sonia","alvaro","09/10/1981","06/06/2024","Mario Reis","3°",1);
-		System.out.println(aluno1.toString());
+		
 	}
 
 
--- src/projetoJavaCurso/Pessoa.java
+++ src/projetoJavaCurso/Pessoa.java
@@ -1,10 +1,13 @@
 package projetoJavaCurso;
 
+import Operacoes_com_objeto_Aluno.Aluno;
+
 import java.util.Date;
 
-public class Pessoa {
+public class Pessoa extends Aluno {
 
-	private String nome,sobrenome,endereco,cpf;
+	private String nome, sobrenome, endereco, cpf;
+
 	public String getEndereco() {
 		return endereco;
 	}
@@ -22,14 +25,13 @@ public class Pessoa {
 	}
 
 	private int idade;
-	private Date dataNascimento;
-	
+	private String dataNascimento;
 
-	public Date getDataNascimento() {
-		return dataNascimento;
+	public String getDataNascimento() {
+		return  dataNascimento;
 	}
 
-	public void setDataNascimento(Date dataNascimento) {
+	public void setDataNascimento(String dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
 
@@ -56,5 +58,11 @@ public class Pessoa {
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-	
+
+	public static void main(String[] args) {
+		Aluno aluno = new Aluno("Bruno", "43899030", "3112245", "sonia", "alvaro", "09/10/1981", "06/06/2024",
+				"Mario Reis", "3°", 1);
+		System.out.println(aluno.toString());
+	}
+
 }
