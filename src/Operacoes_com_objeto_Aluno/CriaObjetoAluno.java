38183ceb74f0accc701404081c4c7d854684c48f
Bruno <you@example.com>
2024-06-07 10:17:21 -0300

    Add lista de alunos reprovados,aprovados,em recuperaçao

--- src/Operacoes_com_objeto_Aluno/CriaObjetoAluno.java
+++ src/Operacoes_com_objeto_Aluno/CriaObjetoAluno.java
@@ -1,28 +1,34 @@
 package Operacoes_com_objeto_Aluno;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JOptionPane;
 
+import constantes.StatusAluno;
+
 public class CriaObjetoAluno extends Aluno {
 	
+	List listaAluno = new ArrayList();
+	Aluno aluno = new Aluno();
 	
-
 	CriaObjetoAluno() {
-
-		this.setDataMatricula(JOptionPane.showInputDialog("digite a data da martricula do aluno"));
-		this.setDataNascimento(JOptionPane.showInputDialog("Digite a data de nascimento do aluno"));
+        
+		
+		aluno.setDataMatricula(JOptionPane.showInputDialog("digite a data da martricula do aluno"));
+		aluno.setDataNascimento(JOptionPane.showInputDialog("Digite a data de nascimento do aluno"));
 		int idade = Integer.parseInt(JOptionPane.showInputDialog("Idade do aluno"));
-		this.setIdade(idade);
-		this.setNome(JOptionPane.showInputDialog("Nome do aluno"));
-		this.setNomeEscola(JOptionPane.showInputDialog("Nome da escola"));
-		this.setNomeMae(JOptionPane.showInputDialog("Nome da mae do aluno"));
-		this.setNomePai(JOptionPane.showInputDialog("Nome do pai do aluno"));
-		this.setNumeroCpf(JOptionPane.showInputDialog("Numero do cpf do aluno"));
-		this.setRegistroGeral(JOptionPane.showInputDialog("Numero do rg do aluno"));
-		this.setSerieMatriculado(JOptionPane.showInputDialog("Série em que o aluno será matriculado"));
+		aluno.setIdade(idade);
+		aluno.setNome(JOptionPane.showInputDialog("Nome do aluno"));
+		aluno.setNomeEscola(JOptionPane.showInputDialog("Nome da escola"));
+		aluno.setNomeMae(JOptionPane.showInputDialog("Nome da mae do aluno"));
+		aluno.setNomePai(JOptionPane.showInputDialog("Nome do pai do aluno"));
+		aluno.setNumeroCpf(JOptionPane.showInputDialog("Numero do cpf do aluno"));
+		aluno.setRegistroGeral(JOptionPane.showInputDialog("Numero do rg do aluno"));
+		aluno.setSerieMatriculado(JOptionPane.showInputDialog("Série em que o aluno será matriculado"));
 		diciplinaENota();
+		listaAluno.add(aluno);
       
 	}
 
@@ -55,32 +61,68 @@ public class CriaObjetoAluno extends Aluno {
 		
 		int somar = JOptionPane.showConfirmDialog(null,"deseja obter a media do aluno ");
 		if(somar == 0) {
-		reprovadoAprovado(nota);
+		reprovadoAprovado(this.getNotas());
 		}
 		else {
 			System.exit(0);
 		}
-
+        
+		listaNotasEDiciplinas(nota,diciplina);
 	}
 
-	public void reprovadoAprovado(List nota) {
+	public boolean reprovadoAprovado(List nota) {
 		double media = 0;
 		JOptionPane.showMessageDialog(null, nota);
 		for (int i = 0; i< nota.size(); i++) {
           
 		    media = media + (Double)nota.get(i);
-		    
-		}
-		/*
-		 * if(mediaAluno(notas[0],notas[1],notas[2]) > 7) {
-		 * System.out.println("Aluno aprovado"); }else {
-		 * System.out.println("Aluno reprovado"); }
-		 */
-
+		 }
+		
+	    boolean status = (media/nota.size()+1> 7  ? true : false);
+	    
+		aprovadoReprovado(listaAluno,media,nota,status);
+		
+		 return status;  
+	}
+	
+	
+	
+	public void  listaNotasEDiciplinas(List notas,List diciplinas) {
+		
+		notas.remove(0);
+		System.out.println("Diciplinas \n "+ diciplinas +" \n notas" + notas + " aluno "+ aluno.toString());
+	}
+	
+	public void aprovadoReprovado( List aluno, double media, List nota,boolean status) {
+		
+		for(int i = 0; i<aluno.size(); i++) {
+			if(status) {
+				
+			}
+				}
 		JOptionPane.showMessageDialog(null,
-				 (media/nota.size()+1> 7  ? "aprovado" : "reprovado"));
+				 (status ? StatusAluno.APROVADO : StatusAluno.REPROVADO));
+		
 		JOptionPane.showMessageDialog(null,  "sua media foi : "+ media/(nota.size()-1));
 	}
+public void separarAlunosAprovadosReprovados(Aluno aprovados,Aluno reprovados,Aluno recuperacao) {
+		
+		HashMap<String,List<Aluno>> maps = new HashMap<String, List <Aluno>>();
+		
+		maps.put(StatusAluno.APROVADO, new ArrayList<Aluno>());
+		maps.get(StatusAluno.APROVADO).add(aprovados);
+		maps.put(StatusAluno.RECUPERACAO , new ArrayList<Aluno>());
+		maps.get(StatusAluno.RECUPERACAO).add(recuperacao);
+		maps.put(StatusAluno.REPROVADO, new ArrayList<Aluno>());
+		maps.get(StatusAluno.REPROVADO).add(reprovados);
+		
+		JOptionPane.showMessageDialog(null,maps.get(aprovados));
+		
+		
+		
+		
+	}
+	
 
 	
 
---
+++ src/constantes/StatusAluno.java
@@ -0,0 +1,8 @@
+package constantes;
+
+public class StatusAluno {
+	
+	public static String REPROVADO = "Reprovado";
+    public static String APROVADO = "Aprovado";
+    public static String RECUPERACAO = "Recupacao";
+}
